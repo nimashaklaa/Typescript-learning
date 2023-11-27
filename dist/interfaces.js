9f4a63eaa04bdaf89bcfd374e8e714cc199a2d24
nimashaklaa <119551535+nimashaklaa@users.noreply.github.com>
2023-11-27 16:17:16 +0530

    c6

--- dist/interfaces.js
+++ dist/interfaces.js
@@ -1,8 +1,23 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const Amandi = { dbId: 22234, email: "amandi@gmail.com", userId: 12334345,
     startTrial: () => {
         return "trail started";
-    }
+    },
+    getCoupen(coupenName = "SamanMal") {
+        return 10;
+    },
+    githubToken: "tAhbshehsHH"
 };
 console.log(Amandi.startTrial());
+const Nilumi = { dbId: 22234, email: "amandi@gmail.com", userId: 12334345,
+    startTrial: () => {
+        return "trail started";
+    },
+    getCoupen(coupenName = "SamanMal") {
+        return 10;
+    },
+    githubToken: "tAhbshehsHH",
+    role: "admin"
+};
 //# sourceMappingURL=interfaces.js.map
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -6,13 +6,44 @@ interface UserNo{
     googleId ?:number;
     //startTrial:()=>string ;
     startTrial():string
+    getCoupen(coupenName:String):number
 
+}
+//***********If I want a github token we do not need to touch the previous interface again===================
+//ReOpening of the interface
+interface UserNo{
+    githubToken:String
 }
 
 const Amandi: UserNo ={dbId: 22234, email:"amandi@gmail.com",userId:12334345,
 startTrial:() =>{
     return "trail started";
-}
-
+},
+getCoupen(coupenName="SamanMal") {
+    return 10  
+},
+githubToken:"tAhbshehsHH"
 }
 console.log(Amandi.startTrial())
+
+//===========..........inheritance.......========================================================================
+interface Admin extends UserNo{
+    role:"admin"|"TA"|"Learner"
+
+}
+const Nilumi: Admin ={dbId: 22234, email:"amandi@gmail.com",userId:12334345,
+startTrial:() =>{
+    return "trail started";
+},
+getCoupen(coupenName="SamanMal") {
+    return 10  
+},
+githubToken:"tAhbshehsHH",
+role:"admin"
+}
+// interface vs type is type cannot be changed after creating but not interface
+//can extend via intercetion in type 
+
+
+
+export{}
